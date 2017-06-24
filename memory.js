1021ac2fe4dc1b1727075cf9ea47d1dba85b54b1
hwwilkes <hwilkes@gmail.com>
2017-06-24 14:37:06 -0400

    Random Images
    Clicking a div now randomly displays 1/10 images.

--- memory.js
+++ memory.js
@@ -1,12 +1,43 @@
 $(document).ready(function() {
 
-	function switchImg() {
+	var images = ["chicken.jpg", "batonbob.jpg", "clermont.jpg", "falcons.jpg", 
+	  "I85.jpg", "kroger.png",  "peach.jpg", "traffic.jpg", "varsity.jpg", "wind.jpg"];
+	var cards = document.getElementsByClassName("cardImg");
+	var allCards = [];
 
-		var clickedImg = document.getElementById("1");
-		clickedImg.src = "images/chicken.jpg";
+	//object constructor for cards
+	function CreateCard (imageName, divID) {
+
+		this.imageName = imageName;
+		this.divID = divID;
 
 	}
-	
-	document.getElementById("1").addEventListener("click", switchImg);
+
+	for (var i = 0; i < cards.length; i++) {
+
+		var randomNum = Math.floor(Math.random() * (images.length));
+		var randomImg = images[randomNum];
+		allCards[i] = new CreateCard (randomImg, i);
+
+    }
+
+	$(".cardImg").click (function () {
+
+		var imgID = parseInt($(this).attr("id"));
+
+		for (var i = 0; i < allCards.length; i++) {
+
+			var allCardsID = allCards[i].divID;
+
+			if (allCardsID === imgID) {
+
+				var imgName = allCards[i].imageName;
+				break;
+			}
+		}
+
+		document.getElementById(imgID).src = "images/" + imgName;
+
+	})
 
 });
